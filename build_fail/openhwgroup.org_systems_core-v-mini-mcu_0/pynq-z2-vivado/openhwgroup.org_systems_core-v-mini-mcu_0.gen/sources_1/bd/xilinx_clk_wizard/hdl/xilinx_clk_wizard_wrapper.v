c4b336d6c03ed591c64049f73a5c92a07411e606
Gonzalo Salinas <gonzalo.salinas@nvision.es>
2024-07-17 11:27:13 +0200

    Added Vivado project
    - SYNTH gives comb lowrisc_prim_flop
    - IMPL does not meet timing

---
+++ build_fail/openhwgroup.org_systems_core-v-mini-mcu_0/pynq-z2-vivado/openhwgroup.org_systems_core-v-mini-mcu_0.gen/sources_1/bd/xilinx_clk_wizard/hdl/xilinx_clk_wizard_wrapper.v
@@ -0,0 +1,24 @@
+//Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2021.2 (lin64) Build 3367213 Tue Oct 19 02:47:39 MDT 2021
+//Date        : Tue Jul 16 11:20:28 2024
+//Host        : gonzo-VirtualBox running 64-bit Ubuntu 20.04.6 LTS
+//Command     : generate_target xilinx_clk_wizard_wrapper.bd
+//Design      : xilinx_clk_wizard_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module xilinx_clk_wizard_wrapper
+   (clk_125MHz,
+    clk_out1_0);
+  input clk_125MHz;
+  output clk_out1_0;
+
+  wire clk_125MHz;
+  wire clk_out1_0;
+
+  xilinx_clk_wizard xilinx_clk_wizard_i
+       (.clk_125MHz(clk_125MHz),
+        .clk_out1_0(clk_out1_0));
+endmodule
