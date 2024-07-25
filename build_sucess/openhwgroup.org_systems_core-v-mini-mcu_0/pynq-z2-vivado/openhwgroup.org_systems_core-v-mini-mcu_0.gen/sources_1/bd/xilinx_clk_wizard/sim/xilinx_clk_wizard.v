b6f3f75c507a7612c44607ea45be0fdb107994a5
Gonzalo Salinas <gonzalo.salinas@nvision.es>
2024-07-25 12:13:07 +0200

    Added third attempt. Synth and impl WORKS
    - No combinational loops
    - Timing is met

---
+++ build_sucess/openhwgroup.org_systems_core-v-mini-mcu_0/pynq-z2-vivado/openhwgroup.org_systems_core-v-mini-mcu_0.gen/sources_1/bd/xilinx_clk_wizard/sim/xilinx_clk_wizard.v
@@ -0,0 +1,27 @@
+//Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2021.2 (lin64) Build 3367213 Tue Oct 19 02:47:39 MDT 2021
+//Date        : Tue Jul 23 14:10:27 2024
+//Host        : gonzo-VirtualBox running 64-bit Ubuntu 20.04.6 LTS
+//Command     : generate_target xilinx_clk_wizard.bd
+//Design      : xilinx_clk_wizard
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+(* CORE_GENERATION_INFO = "xilinx_clk_wizard,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=xilinx_clk_wizard,x_ipVersion=1.00.a,x_ipLanguage=VERILOG,numBlks=1,numReposBlks=1,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=0,numPkgbdBlks=0,bdsource=USER,synth_mode=OOC_per_IP}" *) (* HW_HANDOFF = "xilinx_clk_wizard.hwdef" *) 
+module xilinx_clk_wizard
+   (clk_125MHz,
+    clk_out1_0);
+  (* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 CLK.CLK_125MHZ CLK" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME CLK.CLK_125MHZ, CLK_DOMAIN xilinx_clk_wizard_clk_125MHz, FREQ_HZ 125000000, FREQ_TOLERANCE_HZ 0, INSERT_VIP 0, PHASE 0.0" *) input clk_125MHz;
+  (* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 CLK.CLK_OUT1_0 CLK" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME CLK.CLK_OUT1_0, CLK_DOMAIN /clk_wiz_0_clk_out1, FREQ_HZ 15000000, FREQ_TOLERANCE_HZ 0, INSERT_VIP 0, PHASE 0.0" *) output clk_out1_0;
+
+  wire clk_in1_0_1;
+  wire clk_wiz_0_clk_out1;
+
+  assign clk_in1_0_1 = clk_125MHz;
+  assign clk_out1_0 = clk_wiz_0_clk_out1;
+  xilinx_clk_wizard_clk_wiz_0_0 clk_wiz_0
+       (.clk_in1(clk_in1_0_1),
+        .clk_out1(clk_wiz_0_clk_out1));
+endmodule
---
+++ build_sucess/openhwgroup.org_systems_core-v-mini-mcu_0/pynq-z2-vivado/openhwgroup.org_systems_core-v-mini-mcu_0.gen/sources_1/ip/xilinx_mem_gen_8192/xilinx_mem_gen_8192_stub.vhdl
@@ -0,0 +1,35 @@
+-- Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2021.2 (lin64) Build 3367213 Tue Oct 19 02:47:39 MDT 2021
+-- Date        : Tue Jul 23 14:09:28 2024
+-- Host        : gonzo-VirtualBox running 64-bit Ubuntu 20.04.6 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/gonzo/fpu-fail/x-heep/build/openhwgroup.org_systems_core-v-mini-mcu_0/pynq-z2-vivado/openhwgroup.org_systems_core-v-mini-mcu_0.gen/sources_1/ip/xilinx_mem_gen_8192/xilinx_mem_gen_8192_stub.vhdl
+-- Design      : xilinx_mem_gen_8192
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity xilinx_mem_gen_8192 is
+  Port ( 
+    clka : in STD_LOGIC;
+    ena : in STD_LOGIC;
+    wea : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    addra : in STD_LOGIC_VECTOR ( 12 downto 0 );
+    dina : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end xilinx_mem_gen_8192;
+
+architecture stub of xilinx_mem_gen_8192 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,ena,wea[3:0],addra[12:0],dina[31:0],douta[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_5,Vivado 2021.2";
+begin
+end;
